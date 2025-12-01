09f2dc9728333c2f9eb061f634495ab2240b093b
Mayne <00broskis@gmail.com>
2025-12-01 17:56:14 -0500

    Move hero content further down

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,7 +67,7 @@ function Page() {
           </div>
 
           <div className="hero-overlay"></div>
-          <div className="hero-content-stack mx-auto max-w-6xl px-4 pt-72 pb-24 md:pt-96 md:pb-28">
+          <div className="hero-content-stack mx-auto max-w-6xl px-4 pt-80 pb-24 md:pt-[28rem] md:pb-28">
             <div className="hero-inner hero-inner-merged hero-single-stack space-y-6">
               <div className="hero-crest">
                 <img
